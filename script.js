7fa8162dcce17d414f0ec6ac3cb907a9258f30c3
felipeer <60820855+felipeer@users.noreply.github.com>
2022-09-24 14:01:34 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let contador = 1;
-setInterval(function() {
-    document.getElementById('radio' + contador).checked = true; //função da origem para trocar as imagens
-    contador++
-
-    if (contador > 7) { //quantidade de fotos
-        contador = 1; //retorno para a primeira foto
-    }
-}, 3000)
